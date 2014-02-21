6f390fff60b31a07939e6d325796bdcbfefb5907
Dav Glass <davglass@gmail.com>
2014-02-21 08:37:58 -0600

    Added some better tests

--- tests/test.js
+++ tests/test.js
@@ -1,5 +1,6 @@
 var vows = require('vows'),
     assert = require('assert'),
+    path = require('path'),
     checker = require('../lib/index');
 
 var tests = {
@@ -12,6 +13,37 @@ var tests = {
         },
         'should load print': function(topic) {
             assert.isFunction(topic.print);
+        }
+    },
+    'should parse local with unknown': {
+        topic: function () {
+            var self = this;
+
+            checker.init({
+                start: path.join(__dirname, '../')
+            }, function (sorted) {
+                self.callback(null, sorted);
+            });
+        },
+        'and give us results': function (d) {
+            assert.isTrue(Object.keys(d).length > 70);
+            assert.equal(d['abbrev@1.0.4'].licenses, 'MIT');
+        },
+        'should parse local without unknown': {
+            topic: function () {
+                var self = this;
+
+                checker.init({
+                    start: path.join(__dirname, '../'),
+                    unknown: true
+                }, function (sorted) {
+                    self.callback(null, sorted);
+                });
+            },
+            'and give us results': function (d) {
+                assert.equal(d['vows@0.7.0'].licenses, 'BSD*');
+                assert.isTrue(Object.keys(d).length > 20);
+            }
         }
     }
 };
